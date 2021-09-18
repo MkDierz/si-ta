9a7481a43c6bdcb3fd3f1c0085f0d09c8b6e166a
MkDierz <msyibbran24@gmail.com>
2021-09-18 12:29:06 +0700

    create index for admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,28 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Admin Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3>Selamat datang admin</h3>
+        </div>
+        <div class="card-body">
+            <div>sss</div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
