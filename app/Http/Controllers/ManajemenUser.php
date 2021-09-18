d39221ca52b4d3749addcbb4fcf2df4373ec0f8f
MkDierz <msyibbran24@gmail.com>
2021-09-18 17:50:22 +0700

    create management user controller

---
+++ app/Http/Controllers/ManajemenUser.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class ManajemenUser extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = User::all('id','name','nim_nip','email','role');
+        // dd($data);
+        return view('admin.M_user.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.M_user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        foreach ($request->except('_token') as $data => $value) {
+            $valids[$data] = "required";
+        }
+        $request->validate($valids);
+        $request['password'] = Hash::make($request->password);
+        dd($request->all());
+        User::create($request->all());
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        // dd($user);
+        return view('admin.M_user.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view('admin.M_user.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,User $user)
+    {
+        foreach ($request->except('_token') as $data => $value) {
+            $valids[$data] = "required";
+        }
+        $request->validate($valids);
+
+        $user->update($request->all());
+
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('user.index');
+    }
+}
