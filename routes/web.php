14c8587d031bc8f5b470fd44889f2d320d753411
MkDierz <msyibbran24@gmail.com>
2021-09-20 17:09:32 +0700

    create route for all controller

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ManajemenUser;
 use App\Http\Controllers\DataMahasiswa;
 use App\Http\Controllers\DataDosen;
+use App\Http\Controllers\Mahasiswa;
+use App\Http\Controllers\Dosen;
 
 
 /*
@@ -34,4 +36,23 @@ Route::middleware(['can:admin'])->prefix('admin')->group(function () {
         'mahasiswa' => DataMahasiswa::class,
         'dosen' => DataDosen::class
     ]);
+});
+
+Route::middleware(['can:mahasiswa'])->prefix('mhs')->group(function (){
+    Route::get('', [Mahasiswa::class, 'index'])->name('mhs');
+    
+    Route::get('/kehadiran', [Mahasiswa::class, 'kehadiran'])->name('kehadiran');
+
+    Route::post('/kehadiran', [Mahasiswa::class, 'kehadiran_save'])->name('post.kehadiran');
+    Route::post('/kelayakan', [Mahasiswa::class, 'kelayakan_save'])->name('post.kelayakan');
+});
+
+Route::middleware(['can:pembimbing'])->prefix('dosen')->group(function (){
+    Route::get('', [Dosen::class, 'index'])->name('dosen');
+
+    Route::get('/kelayakan/{id}', [Dosen::class, 'kelayakan'])->name('kelayakan.show');
+    Route::put('/kelayakan/{id}', [Dosen::class, 'kelayakan_store'])->name('kelayakan.store');
+
+    Route::get('/konsultasi/{id}', [Dosen::class, 'konsultasi'])->name('konsultasi.show');
+    Route::put('/konsultasi/{id}', [Dosen::class, 'konsultasi_store'])->name('konsultasi.store');
 });
