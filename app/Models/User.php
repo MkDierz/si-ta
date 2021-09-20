e148c00f73513b99a8c22abe7db46d12333d9117
MkDierz <msyibbran24@gmail.com>
2021-09-21 01:11:43 +0700

    show pict on admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Pembimbing;
+use App\Models\Mahasiswa;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -43,4 +47,21 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function adminlte_image()
+    {
+        $url = 'uploads/';
+        if (Gate::allows('admin')) {
+            return 'https://picsum.photos/300/300';
+        }
+        if (Gate::allows('pembimbing')) {
+            $foto = Pembimbing::where('NIP', Auth::user()->nim_nip)->first('foto')['foto'];
+            return $url.$foto;
+        }
+        if (Gate::allows('mahasiswa')) {
+            $foto = Mahasiswa::where('NIM', Auth::user()->nim_nip)->first('foto')['foto'];
+            return $url.$foto;
+        }
+
+    }
 }
