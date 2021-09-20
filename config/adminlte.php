d4f7ff26e69fae4f79f7b4291137d4353b4e7a08
MkDierz <msyibbran24@gmail.com>
2021-09-21 01:00:05 +0700

    edit sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -65,9 +65,9 @@ return [
     */
 
     'usermenu_enabled' => true,
-    'usermenu_header' => false,
+    'usermenu_header' => true,
     'usermenu_header_class' => 'bg-primary',
-    'usermenu_image' => false,
+    'usermenu_image' => true,
     'usermenu_desc' => false,
     'usermenu_profile_url' => false,
 
@@ -225,98 +225,46 @@ return [
     */
 
     'menu' => [
-        // Navbar items:
         [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
+            'header' => 'Menu'
+        ],
+        //mahasiswa
+        [
+            'text' => 'Home',
+            'route' => 'mhs',
+            'can'  => 'mahasiswa'
         ],
         [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
+            'text' => 'Buat Kehadiran',
+            'route' => 'kehadiran',
+            'can'  => 'mahasiswa'
         ],
-
-        // Sidebar items:
+        //dosen
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'text'       => 'Home',
+            'route' => 'dosen',
+            'can' => 'pembimbing'
         ],
+        //admin
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text'       => 'Home',
+            'route'      => 'admin',
+            'can' => 'admin',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text'       => 'List Mahasiswa',
+            'route'      => 'mahasiswa.index',
+            'can' => 'admin',
         ],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            'text'       => 'List Dosen',
+            'route'      => 'dosen.index',
+            'can'        => 'admin',
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text'       => 'List User',
+            'route'      => 'user.index',
+            'can'        => 'admin',
         ],
     ],
 
